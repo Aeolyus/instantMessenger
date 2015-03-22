87a962ac50124f6157922975ef45bfcebee389f5
Aeolyus <richardhuang.huang@gmail.com>
2015-03-22 01:46:13 -0700

    first commit

---
+++ src/client/ServerTest.java
@@ -0,0 +1,11 @@
+package client;
+
+import javax.swing.JFrame;
+public class ServerTest {
+	public static void main(String[] args) {
+		Server aeolyus = new Server();
+		aeolyus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		aeolyus.startRunning();
+	}
+
+}
