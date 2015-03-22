902db224cbdeb1a7a597b12205b26bd361a58ceb
Aeolyus <richardhuang.huang@gmail.com>
2015-03-22 02:29:32 -0700

    final final

--- src/client/ClientTest.java
+++ src/client/ClientTest.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class ClientTest {
 	public static void main(String[] args){
 		Client charlie;
-		charlie = new Client("127.0.0.1");
+		charlie = new Client("192.168.95.64");
 		charlie.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		charlie.startRunning();
 	}
