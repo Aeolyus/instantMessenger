4f03f837808985e4e827a698db7918714ed924f0
Aeolyus <richardhuang.huang@gmail.com>
2015-03-23 16:50:34 -0700

    commit 4

--- src/client/ClientTest.java
+++ src/client/ClientTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class ClientTest {
 	public static void main(String[] args){
 		Client charlie;
-		charlie = new Client("192.168.95.64");
+		charlie = new Client("192.168.1.6");
 		charlie.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		charlie.startRunning();
 	}
